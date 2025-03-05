e9a75aab715c89207264780d92a029cc22663abb
jothinathan423 <69342804+jothinathan423@users.noreply.github.com>
2025-03-05 12:55:45 +0530

    dffdffddd (#6)
    * first
    
    * jothi2 (#1)
    
    Co-authored-by: poovu1010 <poopoovarsan432@gmail.com>
    
    * first commit
    
    * Rubiga (#4)
    
    * first
    
    * jothinathan2
    
    * jothinathan
    
    * Rubiga (#5)
    
    * first
    
    * jothinathan2
    
    ---------
    
    Co-authored-by: dharsani0309 <dharsanimv@gmail.com>
    Co-authored-by: poovu1010 <poopoovarsan432@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,89 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { AppBar, Toolbar, Typography, Button, Menu, MenuItem, IconButton } from "@mui/material";
+import { Link } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import BusinessIcon from "@mui/icons-material/Business";
+import WorkIcon from "@mui/icons-material/Work";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import logo from "../assets/mind.png";
 
 const Navbar = () => {
+    const [userType, setUserType] = useState(null);
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    useEffect(() => {
+        const storedUserType = localStorage.getItem("userType");
+        if (storedUserType) {
+            setUserType(storedUserType);
+        }
+    }, []);
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem("userType");
+        setUserType(null);
+        handleMenuClose();
+    };
+
     return (
         <AppBar position="static">
             <Toolbar>
+                {/* Logo */}
+                <Link to="/">
+                    <img src={logo} alt="Mindlancer Logo" style={{ height: 40, marginRight: 16 }} />
+                </Link>
+
                 <Typography variant="h6" style={{ flexGrow: 1 }}>
                     Mindlancer
                 </Typography>
-                <Button color="inherit" component={Link} to="/dashboard">Freelancer</Button>
-                <Button color="inherit" component={Link} to="/business-dashboard">Business</Button>
-                <Button component={Link} to="/logout" color="inherit">
-                    Logout
-                </Button>
-                <Button component={Link} to="/post-job" variant="contained" color="primary">
-                    Post a Job
-                </Button>
+
+                {/* Buttons based on user type */}
+                {userType === "freelancer" ? (
+                    <>
+                        <Button color="inherit" component={Link} to="/seek-job">
+                            <WorkIcon style={{ marginRight: 5 }} /> Seek Job
+                        </Button>
+                        <IconButton color="inherit" onClick={handleMenuOpen}>
+                            <AccountCircleIcon />
+                        </IconButton>
+                    </>
+                ) : userType === "business" ? (
+                    <>
+                        <Button color="inherit" component={Link} to="/post-job">
+                            <BusinessIcon style={{ marginRight: 5 }} /> Post Job
+                        </Button>
+                        <IconButton color="inherit" onClick={handleMenuOpen}>
+                            <MoreVertIcon />
+                        </IconButton>
+                    </>
+                ) : (
+                    <>
+                        <Button color="inherit" component={Link} to="/login">
+                            Login
+                        </Button>
+                        <Button color="inherit" component={Link} to="/register">
+                            Register
+                        </Button>
+                    </>
+                )}
+
+                {/* Profile Dropdown */}
+                <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                    <MenuItem component={Link} to="/profile" onClick={handleMenuClose}>
+                        Profile
+                    </MenuItem>
+                    <MenuItem component={Link} to="/settings" onClick={handleMenuClose}>
+                        Settings
+                    </MenuItem>
+                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Menu>
             </Toolbar>
         </AppBar>
     );
